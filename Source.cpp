5e679e2f077d8f9d8703e51a8bdce0ccff67aa26
falcuun <falcuun@gmail.com>
2019-01-19 23:59:44 +0100

    Adding basics for recognizing faces using openCv's haarcasscade frontalface file

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
-#include <opencv2/opencv.hpp>
+#include "opencv2/objdetect/objdetect.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+
+void detectFaces(cv::Mat image);
+
+cv::CascadeClassifier face;
 
 int main()
 {
@@ -7,6 +13,7 @@ int main()
 
 	if (!vc.isOpened())
 		return 0;
+	face.load("haarcascade_frontalface_alt.xml");
 
 	cv::Mat image;
 
@@ -21,5 +28,22 @@ int main()
 			vc.release();
 			break;
 		}
+	}
+}
+
+void detectFaces(cv::Mat image)
+{
+	std::vector<cv::Rect> faces;
+	cv::Mat gray_image;
+
+	cv::cvtColor(image, gray_image, cv::COLOR_BGR2GRAY);
+
+	face.detectMultiScale(gray_image, faces, 1.1, 2, 0, cv::Size(30, 30));
+
+	for (size_t i = 0; i < faces.size(); i++)
+	{
+		cv::Point center(faces[i].x + faces[i].width*0.5, faces[i].y + faces[i].height*0.5);
+		
+		cv::Mat faceROI = gray_image(faces[i]);
 	}
 }
